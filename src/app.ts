4d04e708906d73ca75085593b827934114ca031c
Tanvir Ahmed <tanvir03.2001@gmail.com>
2025-07-14 16:34:48 +0600

    added notFound route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 import { router } from "./app/routes";
 const app = express();
 
@@ -16,5 +17,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
