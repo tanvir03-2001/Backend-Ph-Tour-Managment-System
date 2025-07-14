fdc6307ae5b91c1b39bcce5a3a32ba73701f87ca
Tanvir Ahmed <tanvir03.2001@gmail.com>
2025-07-14 16:22:04 +0600

    added global error helpers and create instance Error class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
-
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import { router } from "./app/routes";
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to Tour Management System Backend",
   });
 });
+
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,46 @@
+import { model, Schema } from "mongoose";
+import { IAuthProvider, IsActive, IUser, Role } from "./user.interface";
+
+const authProviderSchema = new Schema<IAuthProvider>(
+  {
+    provider: { type: String, required: true },
+    providerId: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    _id: false,
+  }
+);
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, require: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    role: {
+      type: String,
+      enum: Object.values(Role),
+      default: Role.USER,
+    },
+    phone: String,
+    picture: String,
+    address: String,
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: String,
+      enum: Object.values(IsActive),
+      default: IsActive.ACTIVE,
+    },
+    isVerified: { type: Boolean, default: false },
+    auths: [authProviderSchema],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const User = model<IUser>("User", userSchema);
