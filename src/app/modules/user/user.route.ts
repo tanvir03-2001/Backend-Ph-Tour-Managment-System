2d7429260f77c40d9e5ccf92a3d253e8715f8b02
Tanvir Ahmed <tanvir03.2001@gmail.com>
2025-07-15 18:32:34 +0600

    added zod validation schema and middleware for zod validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
 import { UserController } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router();
-
-router.post("/register", UserController.createUser);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserController.createUser
+);
 router.get("/all-user", UserController.getAllUsers);
 
 export const UserRoutes = router;
