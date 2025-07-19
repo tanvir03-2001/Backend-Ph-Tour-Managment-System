00ed3b2e417393e9c2676fe4a739fb1ea23c7a11
Tanvir Ahmed <tanvir03.2001@gmail.com>
2025-07-19 18:46:07 +0600

    complete global error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,10 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
+import { handleCastError } from "../helper/handleCastError";
+import { handleDuplicatedError } from "../helper/handleDuplicatedError";
+import { handleValidationError } from "../helper/handleValidationError";
+import { handleZodError } from "../helper/handleZodError";
 
 export const globalErrorHandler = (
   err: any,
@@ -13,7 +17,36 @@ export const globalErrorHandler = (
   let statusCode = 500;
   let message = `Something Want Wrong ${err.message}`;
 
-  if (err instanceof AppError) {
+  let errorSources: any = [];
+
+  // Duplicate error
+  if (err.code === 11000) {
+    const simplifiedError = handleDuplicatedError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+  // Object ID error / cast error
+  else if (err.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+  // Validation error
+  else if (err.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  }
+  // Zod error
+  else if (err.name === "ZodError") {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  }
+  // Custom AppError instance error
+  else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   } else if (err instanceof Error) {
@@ -24,7 +57,7 @@ export const globalErrorHandler = (
   res.status(statusCode).json({
     success: false,
     message,
-    err,
+    err: envVars.NODE_ENV === "development" ? err : null,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
 };
